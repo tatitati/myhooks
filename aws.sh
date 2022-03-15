784ed84694f4216edec25c5986392046de01b8ce
Jose Albert <francis.jaa@protonmail.com>
2022-03-15 16:49:51 +0100

    asdf

--- aws.sh
+++ aws.sh
@@ -24,13 +24,52 @@ aws_who(){
 aws_resources(){
    project=`basename "$PWD"` | tr '[:lower:]' '[:upper:]'
    env=${2:-DEV}   
-   
-   echo "\n\nSTACK: ${project}-${env}"
-   aws cloudformation  describe-stack-resources --stack-name ${project}-${env} | grep "PhysicalResourceId\|ResourceType\|LogicalResourceId"
-   echo "\n\nSTACK: ${project}-${env}-CATALOG"
-   aws cloudformation  describe-stack-resources --stack-name ${project}-${env}-CATALOG | grep "ResourceType\|PhysicalResourceId\|LogicalResourceId"
-   echo "\n\nSTACK: ${project}-CICD-${env}"
-   aws cloudformation  describe-stack-resources --stack-name ${project}-CICD-${env} | grep "ResourceType\|PhysicalResourceId\|LogicalResourceId"
-   echo "\n\nSTACK: ${project}-CODECOMMIT"
-   aws cloudformation  describe-stack-resources --stack-name ${project}-CODECOMMIT | grep "ResourceType\|PhysicalResourceId\|LogicalResourceId"
+
+
+   stackname=${project}-${env}   
+   echo "\n\nSTACK: ${stackname}"   
+   aws cloudformation describe-stacks --stack-name ${stackname} | grep StackId
+   aws cloudformation describe-stack-resources --stack-name ${stackname} | grep "PhysicalResourceId\|ResourceType"
+
+
+   stackname=${project}-${env}-CATALOG
+   echo "\n\nSTACK: ${stackname}"
+   aws cloudformation describe-stacks --stack-name ${stackname} | grep StackId
+   aws cloudformation describe-stack-resources --stack-name ${stackname} | grep "ResourceType\|PhysicalResourceId"
+
+
+   stackname=${project}-CICD-${env}
+   echo "\n\nSTACK: ${stackname}"
+   aws cloudformation describe-stacks --stack-name $stackname | grep StackId
+   aws cloudformation describe-stack-resources --stack-name $stackname | grep "ResourceType\|PhysicalResourceId"
+
+
+   stackname=${project}-CODECOMMIT
+   echo "\n\nSTACK: ${stackname}"
+   aws cloudformation describe-stacks --stack-name $stackname | grep StackId
+   aws cloudformation describe-stack-resources --stack-name $stackname | grep "ResourceType\|PhysicalResourceId"
+}
+
+codepipeline(){
+   env=${2:-DEV}   
+   url="https://eu-west-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${resource}/view?region=eu-west-1"
+   open $url
+}
+
+codepipeline(){   
+   resource=$1
+   url="https://eu-west-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${resource}/view?region=eu-west-1"
+   open $url
+}
+
+s3(){
+   resource=$1
+   url="https://s3.console.aws.amazon.com/s3/buckets/${resource}?region=eu-west-1&tab=objects"
+   open $url
+}
+
+cloudformation(){      
+   resource=$1      
+   url="https://eu-west-1.console.aws.amazon.com/cloudformation/home?region=eu-west-1#/stacks/stackinfo?filteringStatus=active&filteringText=scores&viewNested=true&hideStacks=false&stackId=arn%3Aaws%3Acloudformation%3Aeu-west-1%3A800457644486%3Astack%2F${resource}"
+   open $url
 }
